f994dce2f53fa0c59b6b5fd8ca12772218d5c296
FiliPGms <166773370+FiliPGms@users.noreply.github.com>
2024-06-21 16:39:49 -0300

    Update mongodb.js

--- back-end/src/settings/mongodb.js
+++ back-end/src/settings/mongodb.js
@@ -1,1 +1,13 @@
+const mongoose = require("mongoose")
+require('dotenv').config()
 
+const username= process.env.DB_USER
+const password= process.env.DB_PASSWORD
+const cluster= process.env.DB_CLUSTER
+
+mongoose.connect(`mongodb+srv://${username}:${password}@${cluster}.mi8dm4m.mongodb.net/`).then(()=>{
+    console.log('Conectado ao MongoDB!')
+})
+.catch((err)=>{
+    console.log(err)
+})
