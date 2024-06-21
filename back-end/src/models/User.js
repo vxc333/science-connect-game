d2fd75862d738260ae0e0653b1267af03043543e
FiliPGms <166773370+FiliPGms@users.noreply.github.com>
2024-06-21 20:04:58 -0300

    Update User.js

--- back-end/src/models/User.js
+++ back-end/src/models/User.js
@@ -1,1 +1,29 @@
+const mongoose = require("mongoose")
 
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      role: {
+        type: String,
+        enum: ['student', 'teacher', 'cientist'], 
+        default: 'user',
+      },
+      avatar: {
+        type: String,
+        default: '', //URL aqui
+      }
+}, { timestamps: true });
+
+
+module.exports= User;
