a85e29794ad0b27a4220af72a80f72764dbf4891
Widson Martins <131882974+widson27@users.noreply.github.com>
2024-06-24 17:06:15 -0300

    criando arquivos da landing page

---
+++ front-end/JS/script.js
@@ -0,0 +1,54 @@
+const camposDoFormulario = document.querySelectorAll('[required]')
+const formulario = document.querySelector("[data-formulario]");
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const listaRespostas = {
+        "nome": e.target.elements["nome"].value,
+        "email": e.target.elements["email"].value,
+        "senha": e.target.elements["senha"].value,
+        
+    }
+
+    localStorage.setItem("cadastro", JSON.stringify(listaRespostas));
+
+    window.location.href = './landing-page.html';
+})
+
+const mensagens = {
+    nome: {
+        valueMissing: "O campo de nome não pode estar vazio.",
+        patternMismatch: "Por favor, preencha um nome válido.",
+        tooShort: "Por favor, preencha um nome válido."
+    },
+    email: {
+        valueMissing: "O campo de e-mail não pode estar vazio.",
+        typeMismatch: "Por favor, preencha um email válido.",
+        tooShort: "Por favor, preencha um e-mail válido."
+    },
+    senha: {
+        valueMissing: "O campo de senha não pode estar vazio.",
+        patternMismatch: "Por favor, preencha uma senha válida.",
+        tooShort: "O campo de senha não tem caractéres suficientes."
+    }
+}
+
+function verificaCampo(campo) {
+    let mensagem = "";
+    campo.setCustomValidity('');
+    tiposDeErro.forEach(erro => {
+        if (campo.validity[erro]) {
+            mensagem = mensagens[campo.name][erro];
+            console.log(mensagem);
+        }
+    })
+    const mensagemErro = campo.parentNode.querySelector('.mensagem-erro');
+    const validadorDeInput = campo.checkValidity();
+
+    if (!validadorDeInput) {
+        mensagemErro.textContent = mensagem;
+    }   else {
+        mensagemErro.textContent = "";
+    }
+}
